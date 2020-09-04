a11e6cfc07390cddeff9930168d98a6f29eebcc6
client33 <51166024+client33@users.noreply.github.com>
2020-09-04 15:27:34 +0530

    Updated styles for image container in image grid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -3,12 +3,13 @@ import React from 'react';
 export default function ImageGrid({setSelected, setModal, images}) {
   return (
     <div className="img-grid">
-      {images && images.map((doc, index) => {
+      {images && 
+      images.map((doc, index) => {
         return (
           <div onClick={() => {
             setSelected(index);
             setModal(true);
-          }} key={doc.id} className='img-container'>
+            }} key={doc.id} className='img-container'>
             <img src={doc.url} alt={doc.name} />
           </div>
         );
